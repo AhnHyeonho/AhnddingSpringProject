940072d8e965dadd0fe1397c790be6cde9ad68bb
AhnHyeonho <ahndding@gmail.com>
2021-04-15 19:24:42 +0900

    210415 - initialize project

---
+++ AhnddingSpringProject/src/main/java/com/ahndding/AhnddingSpringProject/AhnddingSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ahndding.AhnddingSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhnddingSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhnddingSpringProjectApplication.class, args);
+	}
+
+}
---
+++ AhnddingSpringProject/src/test/java/com/ahndding/AhnddingSpringProject/AhnddingSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahndding.AhnddingSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhnddingSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
